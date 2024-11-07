6c5fc473f1ff4e96710a630ce3cc11dc99b987ff
Ak <itsdevilak@gmail.com>
2024-11-07 12:23:12 +0530

    Add files via upload

---
+++ web proj/scrp.js
@@ -0,0 +1,13 @@
+// Example for adding some interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    // Smooth scroll for anchor links
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
